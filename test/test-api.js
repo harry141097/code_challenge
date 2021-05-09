954ea25f8378ebc5538fe34d8fe9aa206dd0aeda
harry141097 <hainguyenhaiit@gmail.com>
2021-05-09 12:44:22 +0700

    *update unit test

--- test/test-api.js
+++ test/test-api.js
@@ -1,14 +1,14 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../app');
 chai.should();
 chai.use(chaiHttp);
 
 describe('ĐĂNG NHẬP', function () {
     it('Đăng nhập một user với username và password đã đăng ký và nhận được token!', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .post('/api/auth/login')
-            .send({'username': '6', 'password': '1'})
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send({'username': 'harry', 'password': '123'})
             .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
@@ -21,9 +21,10 @@ describe('ĐĂNG NHẬP', function () {
             });
     });
     it('Đăng nhập một user chưa đăng ký!', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .post('/api/auth/login')
-            .send({'username': 'harry', 'password': '123'})
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send({'username': 'nv1', 'password': '111'})
             .end(function (err, res) {
                 res.should.have.status(500);
                 res.should.be.json;
@@ -33,7 +34,7 @@ describe('ĐĂNG NHẬP', function () {
             })
     });
     it('Đăng nhập mà không truyền tham số!', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .post('/api/auth/login')
             .end(function (err, res) {
                 res.should.have.status(500);
@@ -44,8 +45,9 @@ describe('ĐĂNG NHẬP', function () {
             })
     });
     it('Đăng nhập mà không truyền đủ tham số!', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .post('/api/auth/login')
+            .set('content-type', 'application/x-www-form-urlencoded')
             .send({'username': '6'})
             .end(function (err, res) {
                 res.should.have.status(500);
@@ -59,12 +61,14 @@ describe('ĐĂNG NHẬP', function () {
 
 describe('XEM TẤT CẢ THÀNH VIÊN', function () {
     it('Đăng nhập rồi xem tất cả thành viên trong công ty', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .post('/api/auth/login')
-            .send({'username': '6', 'password': '1'})
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send({'username': 'harry', 'password': '123'})
             .end(function (err, res) {
-                chai.request(server)
+                chai.request('http://localhost:3000')
                     .get('/api/member/get')
+                    .set('content-type', 'application/x-www-form-urlencoded')
                     .set('authorization', res.body.data.token)
                     .end(function (err, res) {
                         res.should.have.status(200);
@@ -78,7 +82,7 @@ describe('XEM TẤT CẢ THÀNH VIÊN', function () {
 
     });
     it('Không đăng nhập mà xem tất cả thành viên trong công ty', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .get('/api/member/get')
             .end(function (err, res) {
                 res.should.have.status(403);
@@ -90,8 +94,9 @@ describe('XEM TẤT CẢ THÀNH VIÊN', function () {
 
     });
     it('Dùng token giả để xem tất cả thành viên trong công ty', function (done) {
-        chai.request(server)
+        chai.request('http://localhost:3000')
             .get('/api/member/get')
+            .set('content-type', 'application/x-www-form-urlencoded')
             .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjYwOTRiYWI1ZTUwYmM1MGJlYzkwMzUxMSIsInVzZXJuYW1lIjoiNiIsInBhc3N3b3JkIjoiMSJ9LCJpYXQiOjE2MjAzOTIyNDIsImV4cCI6MTYyMDM5MzI0Mn0.XFVCo1g9OMwq7uZaSAjHjrfi7z8Emxe_yPXw-eaOyBA')
             .end(function (err, res) {
                 res.should.have.status(401);
